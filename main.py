1324ea5900366b5216378292ef0fe5613d8eb4df
mateo <matedutten@gmail.com>
2024-05-14 13:15:20 -0300

    the ui now can request simple data to the API by get_data function

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
 import streamlit as st
 import pandas as pd
+import requests
 
-def goal_table(time, goals, goal_status, width,):
-    
+
+def get_data(index, type):
+    _url = "http://localhost:8000/goal"
+    r = requests.get(url=_url)
+    data = r.json()
+    goal = data[index][type]
+    return goal
+
+
+data_goal_year = get_data(1, "goal")
+
+
+def goal_table(time, goals, goal_status, width):
     st.write(time)
-    
-    dt = pd.DataFrame( {
-            "Goal Name": [
-                goals
-            ],
-            "Status": [
-                goal_status
-            ]
-        }
-    )
+
+    dt = pd.DataFrame({
+        "Goal Name": [
+            goals
+        ],
+        "Status": [
+            goal_status
+        ]
+    })
     st.data_editor(
         dt,
         width=width,
@@ -23,7 +34,7 @@ def goal_table(time, goals, goal_status, width,):
                 help="select wich goal is **ended**",
                 width="small",
                 default=False,
-        ),
+            ),
             "Goal Name": st.column_config.Column(
                 width="large"
             )},
@@ -31,8 +42,9 @@ def goal_table(time, goals, goal_status, width,):
         hide_index=True,
     )
 
-goal_table("Years", "asd", False ,800,)
 
-goal_table("Months", "asd", False ,600,)
+goal_table("Years", data_goal_year, False, 800, )
 
-goal_table("Weeks", "asdsd", False ,500,)
+goal_table("Months", "asd", False, 600, )
+
+goal_table("Weeks", "asdsd", False, 500, )
